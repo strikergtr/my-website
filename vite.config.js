8f9b69ff169568e24a380566edcabc852fc5f6d1
DESKTOP-6UPT102\StrikerXE10 <cotton989@gmail.com>
2025-09-29 12:46:47 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'strikergtr.github.io/my-website/',
+  base: '/my-website/',
   plugins: [react(),
   VitePWA({
       registerType: 'autoUpdate',
